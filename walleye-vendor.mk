6b7eb71e9055483cddd546f5a5a55c314e028d48
Harsh Shandilya <harsh@prjkt.io>
2018-05-07 11:40:07 +0000

    walleye: Regen
    Adds VZW and camera bits
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- walleye-vendor.mk
+++ walleye-vendor.mk
@@ -24,6 +24,8 @@ PRODUCT_COPY_FILES += \
     vendor/google/walleye/proprietary/etc/firmware/music_detector.sound_model:system/etc/firmware/music_detector.sound_model \
     vendor/google/walleye/proprietary/etc/permissions/com.android.sdm.plugins.connmo.xml:system/etc/permissions/com.android.sdm.plugins.connmo.xml \
     vendor/google/walleye/proprietary/etc/permissions/com.android.sdm.plugins.sprintdm.xml:system/etc/permissions/com.android.sdm.plugins.sprintdm.xml \
+    vendor/google/walleye/proprietary/etc/permissions/com.android.vzwomatrigger.xml:system/etc/permissions/com.android.vzwomatrigger.xml \
+    vendor/google/walleye/proprietary/etc/permissions/com.google.android.camera.experimental2017.xml:system/etc/permissions/com.google.android.camera.experimental2017.xml \
     vendor/google/walleye/proprietary/etc/permissions/com.verizon.apn.xml:system/etc/permissions/com.verizon.apn.xml \
     vendor/google/walleye/proprietary/etc/permissions/com.verizon.embms.xml:system/etc/permissions/com.verizon.embms.xml \
     vendor/google/walleye/proprietary/etc/permissions/com.verizon.llkagent.xml:system/etc/permissions/com.verizon.llkagent.xml \
@@ -81,6 +83,7 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     VerizonUnifiedSettings \
     cneapiclient \
+    com.google.android.camera.experimental2017 \
     com.quicinc.cne.api-V1.0-java \
     com.quicinc.cne \
     embmslibrary \
